1659da47e0ffead1ff623f86fdd0ceb96b221f66
Wikijito7 <wikyfg@gmail.com>
2024-09-14 13:58:20 +0200

    Added MessageProcessorService injection

--- src/main/kotlin/di/ServicesModule.kt
+++ src/main/kotlin/di/ServicesModule.kt
@@ -2,10 +2,13 @@ package di
 
 import es.wokis.dispatchers.AppDispatchers
 import es.wokis.services.config.ConfigService
+import es.wokis.services.processor.MessageProcessorService
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val servicesModule = module {
     singleOf(::ConfigService)
+    singleOf(::MessageProcessorService)
+
     single { AppDispatchers() }
 }
