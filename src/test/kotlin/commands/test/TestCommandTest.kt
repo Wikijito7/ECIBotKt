ca1ecdc7432918332702723de2efdb9b7f59d8b2
wikijito7 <wikyfg@gmail.com>
2025-02-15 13:37:19 +0100

    Created tests

---
+++ src/test/kotlin/commands/test/TestCommandTest.kt
@@ -0,0 +1,105 @@
+package commands.test
+
+import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.entity.interaction.ChatInputCommandInteraction
+import dev.kord.rest.builder.interaction.GlobalMultiApplicationCommandBuilder
+import dev.kord.rest.builder.interaction.string
+import es.wokis.commands.CommandsEnum
+import es.wokis.commands.test.TestCommand
+import es.wokis.services.lavaplayer.GuildLavaPlayerService
+import es.wokis.services.queue.GuildQueueService
+import es.wokis.utils.getMemberVoiceChannel
+import io.mockk.*
+import kotlinx.coroutines.test.runTest
+import mock.*
+import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
+
+class TestCommandTest {
+
+    private val guildQueueService: GuildQueueService = mockk()
+
+    private val testCommand = TestCommand(guildQueueService)
+
+    @Test
+    @Ignore("Mockk fails")
+    /*
+        Verification failed: call 2 of 2: List(child of #2#3).add(any())) was not called
+        java.lang.AssertionError: Verification failed: call 2 of 2: List(child of #2#3).add(any())) was not called
+            at io.mockk.impl.recording.states.VerifyingState.failIfNotPassed(VerifyingState.kt:63)
+            at io.mockk.impl.recording.states.VerifyingState.recordingDone(VerifyingState.kt:42)
+            at io.mockk.impl.recording.CommonCallRecorder.done(CommonCallRecorder.kt:47)
+            at io.mockk.impl.eval.RecordedBlockEvaluator.record(RecordedBlockEvaluator.kt:63)
+            at io.mockk.impl.eval.VerifyBlockEvaluator.verify(VerifyBlockEvaluator.kt:30)
+            at io.mockk.MockKDsl.internalVerify(API.kt:120)
+            at io.mockk.MockKKt.verify(MockK.kt:218)
+            at io.mockk.MockKKt.verify$default(MockK.kt:209)
+            at commands.test.TestCommandTest.Given command When onRegisterCommand is called Then register test command(TestCommandTest.kt:32)
+            at java.base/java.lang.reflect.Method.invoke(Method.java:569)
+            at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
+            at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
+     */
+    fun `Given command When onRegisterCommand is called Then register test command`() {
+        // Given
+        val commandBuilder: GlobalMultiApplicationCommandBuilder = mockk {
+            every { commands } returns mockk {
+                every { add(any()) } returns true
+            }
+        }
+
+        // When
+        testCommand.onRegisterCommand(commandBuilder)
+
+        // Then
+        verify(exactly = 1) {
+            commandBuilder.input(name = CommandsEnum.TEST.commandName, description = "test test") {
+                string(name = "pepe", description = "popopo") {
+                    required = true
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Given command When onExecute is called Then execute command`() {
+        runTest {
+            // Given
+            val mockedStrings: Map<String, String> = mapOf("pepe" to "asd")
+            val interaction: ChatInputCommandInteraction = mockk {
+                every { kord } returns mockedKord
+                every { channel } returns mockedTextChannel
+                every { command } returns mockk {
+                    every { strings } returns mockedStrings
+                }
+            }
+            val lavaPlayerService = mockk<GuildLavaPlayerService> {
+                justRun { loadAndPlay(any()) }
+            }
+
+            coEvery {
+                interaction.getMemberVoiceChannel(mockedKord)
+            } returns mockedVoiceChannel
+
+            every {
+                guildQueueService.getOrCreateLavaPlayerService(
+                    guildId = any(),
+                    textChannel = any(),
+                    voiceChannel = mockedVoiceChannel
+                )
+            } returns lavaPlayerService
+
+            // When
+            testCommand.onExecute(interaction, mockedResponse)
+
+            // Then
+            verify(exactly = 1) {
+                guildQueueService.getOrCreateLavaPlayerService(
+                    guildId = any(),
+                    textChannel = any(),
+                    voiceChannel = mockedVoiceChannel
+                )
+                lavaPlayerService.loadAndPlay("asd")
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/services/commands/CommandHandlerServiceTest.kt
@@ -0,0 +1,60 @@
+package services.commands
+
+import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
+import dev.kord.core.entity.interaction.ChatInputCommandInteraction
+import dev.kord.rest.builder.interaction.GlobalMultiApplicationCommandBuilder
+import es.wokis.commands.CommandsEnum
+import es.wokis.commands.test.TestCommand
+import es.wokis.services.commands.CommandHandlerServiceImpl
+import io.mockk.*
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+
+class CommandHandlerServiceTest {
+
+    private val testCommand: TestCommand = mockk()
+
+    private val commandHandlerService = CommandHandlerServiceImpl(
+        testCommand = testCommand
+    )
+
+    @Test
+    fun `When onRegisterCommand is called Then register all commands`() {
+        // Given
+        val commandBuilder: GlobalMultiApplicationCommandBuilder = mockk {
+            every { commands } returns mockk {
+                every { add(any()) } returns true
+            }
+        }
+        justRun { testCommand.onRegisterCommand(any()) }
+
+        // When
+        commandHandlerService.onRegisterCommand(commandBuilder)
+
+        // Then
+        verify(exactly = 1) {
+            testCommand.onRegisterCommand(commandBuilder)
+        }
+    }
+
+    @Test
+    fun `Given test command When onExecute is called Then execute TestCommand`() = runTest {
+        // Given
+        val commandName = CommandsEnum.TEST.commandName
+        val interaction: ChatInputCommandInteraction = mockk {
+            every { command } returns mockk {
+                every { rootName } returns commandName
+            }
+        }
+        val response: DeferredPublicMessageInteractionResponseBehavior = mockk()
+        coJustRun { testCommand.onExecute(any(), any()) }
+
+        // When
+        commandHandlerService.onExecute(interaction, response)
+
+        // Then
+        coVerify(exactly = 1) {
+            testCommand.onExecute(interaction, response)
+        }
+    }
+}
