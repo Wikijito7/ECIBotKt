b0a39dbad59723aaa81afbc40caa4167e93be939
wikijito7 <wikyfg@gmail.com>
2025-02-25 18:51:23 +0100

    Added more tests

--- src/test/kotlin/commands/play/PlayCommandTest.kt
+++ src/test/kotlin/commands/play/PlayCommandTest.kt
@@ -144,4 +144,44 @@ class PlayCommandTest {
             lavaPlayerService.loadAndPlayMultiple(listOf(url))
         }
     }
+
+    @Test
+    fun `Given command without url When onExecute is called Then play url`() = runTest {
+        // Given
+        val mockedStrings: Map<String, String> = emptyMap()
+        val interaction: ChatInputCommandInteraction = mockk {
+            every { kord } returns mockedKord
+            every { channel } returns mockedTextChannel
+            every { command } returns mockk {
+                every { strings } returns mockedStrings
+            }
+        }
+        val lavaPlayerService = mockk<GuildLavaPlayerService> {
+            justRun { loadAndPlayMultiple(any()) }
+        }
+
+        coEvery {
+            interaction.getMemberVoiceChannel(mockedKord)
+        } returns mockedVoiceChannel
+
+        coEvery {
+            guildQueueService.getOrCreateLavaPlayerService(
+                interaction = interaction
+            )
+        } returns lavaPlayerService
+        every { interaction.guildLocale } returns Locale.ENGLISH_UNITED_STATES
+        every { localizationService.getString(any(), any()) } returns ""
+        every { localizationService.getStringFormat(any(), any(), *anyVararg()) } returns ""
+
+        // When
+        playCommand.onExecute(interaction, mockedResponse)
+
+        // Then
+        coVerify(exactly = 0) {
+            guildQueueService.getOrCreateLavaPlayerService(
+                interaction = interaction
+            )
+            lavaPlayerService.loadAndPlayMultiple(any())
+        }
+    }
 }
