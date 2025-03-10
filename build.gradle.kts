c459646a943402a872e23d07736b2fe70302648b
wikijito7 <wikyfg@gmail.com>
2025-03-10 18:45:47 +0100

    Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -152,9 +152,9 @@ tasks {
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
         manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
         val sourcesMain = sourceSets.main.get()
-        val contents = configurations.runtimeClasspath.get()
-            .map { if (it.isDirectory) it else zipTree(it) } +
-                sourcesMain.output
+        val contents = configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        } + sourcesMain.output
         from(contents)
     }
     build {
